db0ffacc33937315ff5f3b5e4eae013d15b9dcaa
Toby <tobyswjames@gmail.com>
2018-03-01 20:09:04 +0000

    sorted jobs

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ taxis = [cab() for i in range(firstline([2]))]
 
 jobs = rest()
 
-jobs = sorted(jobs, key = lambda x : distance([x[0], x[1]], [x[2], x[3]]))
+jobs_by_length = sorted(jobs, key = lambda x : distance([x[0], x[1]], [x[2], x[3]]))
 
+
+jobs_by_start = sorted(jobs, key = lambda x : x[4])
 
