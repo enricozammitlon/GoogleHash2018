f07c0976ad5911ae4bd55fbfad69e151175c7560
Enrico <enrico.zammitl@gmail.com>
2018-03-01 20:40:10 +0000

    Fixed redundancy

--- cab.py
+++ cab.py
@@ -4,12 +4,10 @@ Created on Thu Mar  1 18:58:41 2018
 @authors: enrico (@rustychickenleg),toby (@tobyjamez)
 """
 class cab:
-    def __init__(self, start_loc, end_loc, steps_left):
-        self.x = start_loc[0]
-        self.y = start_loc[1]
+    def __init__(self):
+        self.x = 0
+        self.y = 0
         self.available = True
-        self.end_loc = end_loc
-        self.steps_left = steps_left
 
     def moveTo(self, end_loc):
         self.x = end_loc[0]
