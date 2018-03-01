7670efecd5c20c709abe6c90f279f3317c17d270
GeorgeDaniel <munteanu.daniel97@gmail.com>
2018-03-01 19:13:42 +0000

    Add files via upload
    Its pretty cool

---
+++ point.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 18:58:41 2018
+
+@author: mbaxagm2
+"""
+class point :
+
+   def __init__(self, xStart, yStart, xFinish, yFinish, earliestStart, latestFinish):
+      self.xStart = xStart
+      self.yStart = yStart
+      
+      self.xFinish = xFinish
+      self.yFinish = yFinish
+      
+      self.earliestStart =  earliestStart
+      self.latestFinish =  latestFinish
+
