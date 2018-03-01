db16717b83017edd33bbc5ce763155aec177364a
Enrico <enrico.zammitl@gmail.com>
2018-03-01 19:32:06 +0000

    Defined cab functions

--- cab.py
+++ cab.py
@@ -1,1 +1,19 @@
-# Teletype ID 331aa478-fc6d-406f-8a07-c9dd60c7950f
+"""
+Created on Thu Mar  1 18:58:41 2018
+
+@authors: enrico (@rustychickenleg),toby (@tobyjamez)
+"""
+class cab:
+    def __init__(self, start_loc, end_loc, steps_left):
+        self.x = start_loc[0]
+        self.y = start_loc[1]
+        self.available = True #boolean
+        self.end_loc = end_loc
+        self.steps_left = steps_left
+
+    def moveTo(self, end_loc):
+        while self.x != end_loc[0]:
+            if self.x < end_loc[0]:
+                self.x += 1
+            else:
+                self.x -= 1
