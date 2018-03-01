5f439d9bfcfb3fd41ba93b74a2115cc5cc093db0
Enrico <enrico.zammitl@gmail.com>
2018-03-01 19:46:01 +0000

    update fix

--- metric.py
+++ metric.py
@@ -1,4 +1,5 @@
-import math
-
 def distance(loc_one,loc_two):
-    dist=math.abs(loc_one[0]-loc_two[0])-math.abs(loc_one[1]-loc_two[1])
+    dist=abs(loc_one[0]-loc_two[0])+abs(loc_one[1]-loc_two[1])
+    return dist
+
+print(distance([3,6],[7,8]))
