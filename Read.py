b79a60e0113cea6b3a1fd0317f4a04a66bbe34fa
Enrico <enrico.zammitl@gmail.com>
2018-03-01 20:28:30 +0000

    Added job return functions

--- Read.py
+++ Read.py
@@ -1,46 +1,43 @@
+class Read:
 
 
-readFile = open('Test.txt','r');
-startx = []
-endx = []
-starty = []
-endy = []
-rides = None
-vehicles = None
-bonus = None
-steps = []
-rows = None
-columns = None
-estart = []
-efinish = []
-steps = None
-processTheLine = 0;
-firstline=""
-for line in readFile:
-    splitUp = line.split();
-    if processTheLine:
-        startx.append(int(splitUp[0]))
-        starty.append(int(splitUp[1]))
-        endx.append(int(splitUp[2]))
-        endy.append(int(splitUp[3]))
-        estart.append(int(splitUp[4]))
-        efinish.append(int(splitUp[5]))
-        
-    else:
-        firstline=line
-        rows = int(splitUp[0])
-        columns = int(splitUp[1])
-        vehicles = int(splitUp[2])
-        rides = int(splitUp[3])
-        bonus = int(splitUp[4])
-        steps = int(splitUp[5]) 
-        processTheLine = 1;
-    
+    def __init__(self,filename):
+        readFile = open(filename,'r');
+        startx = []
+        endx = []
+        starty = []
+        endy = []
+        rides = None
+        vehicles = None
+        bonus = None
+        steps = []
+        rows = None
+        columns = None
+        estart = []
+        efinish = []
+        steps = None
+        processTheLine = 0;
+        self.firstline=""
+        self.jobs=[[]]
+
+        for line in readFile:
+            splitUp = line.split();
+            if processTheLine:
+                self.jobs.append([int(splitUp[0]),int(splitUp[1]),int(splitUp[2]),int(splitUp[3]),int(splitUp[4]),int(splitUp[5])])
+            else:
+                firstline=line
+                rows = int(splitUp[0])
+                columns = int(splitUp[1])
+                vehicles = int(splitUp[2])
+                rides = int(splitUp[3])
+                bonus = int(splitUp[4])
+                steps = int(splitUp[5])
+                processTheLine = 1;
+        readFile.close();
 
 
-readFile.close();
+    def getFirstLine(self):
+        return self.firstline
 
-
-def getFirstLine():
-    return firstline
-print(getFirstLine())
+    def getJobs(self):
+        return self.jobs
--- metric.py
+++ metric.py
@@ -1,3 +1,5 @@
+from Read import *
+
 def distance(loc_one,loc_two):
     dist=abs(loc_one[0]-loc_two[0])+abs(loc_one[1]-loc_two[1])
     return dist
