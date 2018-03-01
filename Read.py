f44bae89f148e7b6dc505a2d446cd88942a5c279
Enrico <enrico.zammitl@gmail.com>
2018-03-01 20:35:11 +0000

    Fixed redundancies

--- Read.py
+++ Read.py
@@ -1,22 +1,7 @@
 class Read:
-
-
     def __init__(self,filename):
         readFile = open(filename,'r');
-        startx = []
-        endx = []
-        starty = []
-        endy = []
-        rides = None
-        vehicles = None
-        bonus = None
-        steps = []
-        rows = None
-        columns = None
-        estart = []
-        efinish = []
-        steps = None
-        processTheLine = 0;
+        processTheLine= 0
         self.firstline=""
         self.jobs=[[]]
 
@@ -26,12 +11,6 @@ class Read:
                 self.jobs.append([int(splitUp[0]),int(splitUp[1]),int(splitUp[2]),int(splitUp[3]),int(splitUp[4]),int(splitUp[5])])
             else:
                 firstline=line
-                rows = int(splitUp[0])
-                columns = int(splitUp[1])
-                vehicles = int(splitUp[2])
-                rides = int(splitUp[3])
-                bonus = int(splitUp[4])
-                steps = int(splitUp[5])
                 processTheLine = 1;
         readFile.close();
 
